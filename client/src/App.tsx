b4ed7c3f819ec67a4b204090000bfef74960a39d
dangrichardyang <richarddangyang@gmail.com>
2023-07-16 00:05:37 +0000

    add: dockerfiles, tailwindcss, and simple login form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,38 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(`username: ${username}, password: ${password}`)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="grid place-items-center min-h-screen min-w-screen">
+        <form className="border flex flex-col w-[30rem] h-[20rem]" onSubmit={handleSubmit}>
+          <div className="flex-1 flex flex-col justify-center px-10 py-10 gap-10 w-full text-center">
+            <h1 className="text-xl font-bold">JWT Login</h1>
+            <input 
+              type="text" 
+              placeholder="Username" 
+              className="border p-4"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}/>
+            <input 
+              type="password" 
+              placeholder="Password" 
+              className="border p-4" 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}/>
+          </div>
+          <button className="bg-green-500 hover:bg-green-600 p-4 w-full">Sign In</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dns from 'dns'
 
-// https://vitejs.dev/config/
+dns.setDefaultResultOrder('verbatim')
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 3000,
+    strictPort: true,
+  }
 })
