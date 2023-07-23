94ef531224a26544d44b171959bbd31c0cacfa92
dangrichardyang <richarddangyang@gmail.com>
2023-07-23 21:42:57 +0000

    auth endpoints working...

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,14 +9,16 @@ function App() {
     e.preventDefault()
     const data = { username, password }
     try {
-      const res = await axios.post('/login/api/login', data);
+      const res = await axios.post('/login/api/login', data, { withCredentials: true });
       console.log(res.data)
+      const prevURL = document.cookie.match('(^|;)\\s*' + "prevURL" + '\\s*=\\s*([^;]+)')?.pop() || 'http://10.0.0.31/login'
+      document.cookie = "prevURL=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"
+      return location.href = prevURL
     } catch (error) {
       console.log(error)
     }
     
   }
-
   return (
     <>
       <div className="grid place-items-center min-h-screen min-w-screen">
--- server/app/main.py
+++ server/app/main.py
@@ -1,9 +1,30 @@
 import secrets
+import redis
+import json
 
-from fastapi import FastAPI, Response, Request
+from datetime import datetime, timezone, timedelta
+from fastapi import FastAPI, Response, Request, HTTPException, status
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 app = FastAPI()
+db = redis.Redis(host="redis")
+
+origins = [
+#    "http://localhost",
+#    "http://localhost:3000",
+    "http://10.0.0.31",
+    "http://10.0.0.31:3000",
+#    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Credentials(BaseModel):
   username: str
@@ -11,20 +32,44 @@ class Credentials(BaseModel):
 
 @app.get("/")
 async def home():
-  return "Hello World"
+  return "Hello World 2"
 
-@app.post("/login/")
+@app.post("/login")
 async def login(data: Credentials, response: Response):
+  error = HTTPException(status_code=401, detail="Incorrect username and/ or password")
+
+  if not data.username or not data.password: raise error
+
+  user = data.username.lower()
+  if user != "user" or data.password != "pass": raise error
+
   token = secrets.token_urlsafe(32)
+  expires = datetime.now(timezone.utc) + timedelta(seconds=10)
+  user_info = json.dumps({"user": user, "roles": ["user"]})
+  db.set(name=token, value=user_info, exat=expires)
   response.set_cookie(
     httponly=True,
     key="secret_token",
     value=token,
-    expires=10
+    expires=expires
   )
-  return { "username": data.username, "secret_token": token }
+  return { "detail": "You have successfully logged in"}
 
-@app.get("/token/")
-async def check_token(request: Request):
+@app.get("/user")
+async def get_user_info(request: Request):
+  error = HTTPException(status_code=403, detail="You are not authooized")
   token = request.cookies.get("secret_token")
-  return { "secret_token": token }
+  if not token: raise error
+
+  data = db.get(name=token)
+  if not data: raise error
+
+  user_info = json.loads(data)
+  return { "user_info": user_info }
+
+@app.get("/logout")
+async def logout(request: Request, response: Response):
+  token = request.cookies.get("secret_token")
+  if token: db.delete(token)
+  response.delete_cookie(key="secret_token")
+  return { "detail": "You have been successfully logged out"}
